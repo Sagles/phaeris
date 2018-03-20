0cb99590264fd6cccab805db498d5b08aa16fb5d
Sagles <37480334+Sagles@users.noreply.github.com>
2018-03-20 20:05:27 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -1,3 +1,14 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    bot.user.setGame("RP Wakfu/Dofus");
+    console.log("Le bot a bien ete connecte");
+});
+
+bot.login(process.env.TOKEN)
+
 // Discord.js
 var Discord = require("discord.js");
 //require("node-opus");
