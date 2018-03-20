457797f7237bc1c923d519df6bd670ab43236964
Sagles <37480334+Sagles@users.noreply.github.com>
2018-03-20 20:55:11 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,31 +9,30 @@ bot.on("ready", function() {
 
 bot.login(process.env.TOKEN);
 
-// Discord.js
+
 var Discord = require("discord.js");
-//require("node-opus");
+
 require("opusscript");
 
-// autoReconnect is enabed
+
 var bot = new Discord.Client({autoReconnect: true});
 
 
-// Set the prefix
+
 let prefix = "/";
 
-// Ready? Set? Go!
+
 bot.on('ready', () => {
 
-  bot.user.setStatus("online"); //dnd , online , ldle
+  bot.user.setStatus("online"); 
   bot.user.setGame("Un jeux ici");
-  //bot.user.setGame("In Dev");
+
   console.log("Newston ! Le bot est en ligne");
 });
 
 
 bot.on("message", msg => {
 
-      // Command /help
      if (msg.content.startsWith(prefix + "help")) {
       msg.channel.sendMessage("```List of commands : \n\n /version  | Version  \n /help | Toute les commandes \n    ```");
       console.log("Command executed: /help")
